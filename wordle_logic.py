490871ab3a2249abc29146f6d08d489435068f49
Adam Essaydi <adamess123@gmail.com>
2023-10-07 15:19:03 -0400

    Separated main file into more digestible pieces, a few bugs but will fix next push

---
+++ wordle_logic.py
@@ -0,0 +1,24 @@
+from termcolor import cprint
+from pyfiglet import figlet_format
+from api_request import fetch_random_word
+from word_coloring import word_coloring
+import sys
+from colorama import init
+
+api_url = 'https://api.api-ninjas.com/v1/randomword'
+api_key = '5mZJECPTxfUjrteYfn47zg==qRVV0l9npNg4rL9p'
+
+
+def run_wordle_game():
+    init(strip=not sys.stdout.isatty())
+    cprint(figlet_format('WORDLE', font='starwars'), 'yellow', 'on_blue', attrs=['bold'])
+
+    word = fetch_random_word(api_url, api_key)
+
+    print("Given Word: " + word)
+
+    word_coloring(word)
+
+    # Every time a green letter is flagged, check entire green_check list
+    # To see if all letters are flagged green. If so, user has won.
+
