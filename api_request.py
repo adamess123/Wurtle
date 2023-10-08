0104aa9d6a488a0d9747ab2db474509540306ea6
Dan <111514188+danmigus@users.noreply.github.com>
2023-10-07 22:28:02 -0500

    Added unicorn while waiting for API

--- api_request.py
+++ api_request.py
@@ -2,11 +2,25 @@ import requests
 
 
 def fetch_random_word(api_url, api_key):
+    print("""\
+
+                        ._ o o    /
+                        \_`-)|_  /
+                    ,""       \ /
+                    ,"     |   ಠ ಠ. 
+                ,"      ,-\__    `.
+                ,"       /     `--._;)
+            ,"        /
+            ,"         / 
+    """)
+    print("Waiting on API", end ="")
     while True:
+        print("..")
         response = requests.get(api_url, headers={
             'X-Api-Key': api_key
         }).json()
 
         if len(response["word"]) == 5:
             word = response["word"].lower()
+            print()
             return word
