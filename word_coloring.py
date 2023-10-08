dae0fc65fccaf25b49381ea05af5d07b27116912
Adam Essaydi <adamess123@gmail.com>
2023-10-07 23:17:24 -0400

    Added consecutive printing

--- word_coloring.py
+++ word_coloring.py
@@ -1,5 +1,5 @@
 from colorama import Fore
-from termcolor import cprint
+from termcolor import cprint, colored
 from pyfiglet import figlet_format
 from rich.console import Console
 from rich.table import Table
@@ -14,8 +14,7 @@ def word_coloring(word):
         if not previous_guesses:
             print("")
         else:
-            console.print(previous_guesses, end="")
-
+            console.print(previous_guesses)
         print(Fore.WHITE)
 
         green_check = []
@@ -40,44 +39,45 @@ def word_coloring(word):
                 break
 
         # Pre check count decrease before printing
-        for element in range(0, len(word)):
-            if word[element] == val[element]:
-                letters[val[element]] -= 1
+        for i in range(0, len(word)):
+            if word[i] == val[i]:
+                letters[val[i]] -= 1
 
-        for element in range(0, len(word)):
-            colored_letters=[]
+        for i in range(0, len(word)):
+            colored_letters=""
             # If letter lines up in the same place, print green.
-            if word[element] == val[element]:
-                green_check[element] = 1
-                console.print("[black on green]" + val[element] + "[/]", end="")
-                colored_letters.append("[black on green]" + val[element] + "[/]")
+            if word[i] == val[i]:
+                green_check[i] = 1
+                console.print("[black on green]" + val[i] + "[/]", end="")
+                guess = colored(val[i], 'black', 'on_green')
+                colored_letters += guess
 
             else:
-                if letters.get(val[element], 0) == 0:
-                    console.print("[black on white]" + val[element] + "[/]", end="")
-                    colored_letters.append("[black on white]" + val[element] + "[/]")
-                    if val[element] not in incorrect_letters:
-                        incorrect_letters.append(val[element].lower())
+                if letters.get(val[i], 0) == 0:
+                    console.print("[black on white]" + val[i] + "[/]", end="")
+                    guess = colored(val[element], 'black', 'on_white')
+                    colored_letters += guess
+                    if val[i] not in incorrect_letters:
+                        incorrect_letters.append(val[i].lower())
 
-                elif letters[val[element]] >= 1:
-                    console.print("[black on yellow]" + val[element] + "[/]", end="")
-                    colored_letters.append("[black on yellow]" + val[element] + "[/]")
+                elif letters[val[i]] >= 1:
+                    console.print("[black on yellow]" + val[i] + "[/]", end="")
+                    guess = colored(val[i], 'black', 'on_yellow')
+                    colored_letters += guess
                 # If yellow is not flagged, then only other option must be white/incorrect letter.
                 else:
-                    console.print("[black on white]" + val[element] + "[/]", end="")
-                    colored_letters.append("[black on white]" + val[element] + "[/]")
-                    if val[element] not in incorrect_letters:
-                        incorrect_letters.append(val[element].lower())
-            for letter in range(0, len(colored_letters)):
-                console.print(colored_letters, end="")
+                    console.print("[black on white]" + val[i] + "[/]", end="")
+                    guess = colored(val[i], 'black', 'on_white')
+                    colored_letters+=guess
+                    if val[i] not in incorrect_letters:
+                        incorrect_letters.append(val[i].lower())
             previous_guesses.append(colored_letters)
 
-
+                # Every time a green letter is flagged, check entire green_check list
+                # To see if all letters are flagged green. If so, user has won.
         print("\nYou have used the following incorrect letters: ")
         print(', '.join(incorrect_letters))
 
-        # Every time a green letter is flagged, check entire green_check list
-        # To see if all letters are flagged green. If so, user has won.
         for check in range(0, len(word)):
             if green_check[check] == 0:
                 losses += 1
