c081f19b8399544d600ece9c8657c87f25922398
Adam Essaydi <adamess123@gmail.com>
2023-10-07 23:00:17 -0400

    Basic logic for printing out previous attempts

--- word_coloring.py
+++ word_coloring.py
@@ -10,6 +10,11 @@ def word_coloring(word):
     losses = 0
     incorrect_letters=[]
     for element in range(0, 6):
+        previous_guesses = []
+        if not previous_guesses:
+            print("")
+        else:
+            console.print(previous_guesses, end="")
 
         print(Fore.WHITE)
 
@@ -40,31 +45,39 @@ def word_coloring(word):
                 letters[val[element]] -= 1
 
         for element in range(0, len(word)):
+            colored_letters=[]
             # If letter lines up in the same place, print green.
             if word[element] == val[element]:
                 green_check[element] = 1
                 console.print("[black on green]" + val[element] + "[/]", end="")
+                colored_letters.append("[black on green]" + val[element] + "[/]")
 
             else:
                 if letters.get(val[element], 0) == 0:
                     console.print("[black on white]" + val[element] + "[/]", end="")
+                    colored_letters.append("[black on white]" + val[element] + "[/]")
                     if val[element] not in incorrect_letters:
                         incorrect_letters.append(val[element].lower())
 
                 elif letters[val[element]] >= 1:
                     console.print("[black on yellow]" + val[element] + "[/]", end="")
+                    colored_letters.append("[black on yellow]" + val[element] + "[/]")
                 # If yellow is not flagged, then only other option must be white/incorrect letter.
                 else:
                     console.print("[black on white]" + val[element] + "[/]", end="")
+                    colored_letters.append("[black on white]" + val[element] + "[/]")
                     if val[element] not in incorrect_letters:
                         incorrect_letters.append(val[element].lower())
+            for letter in range(0, len(colored_letters)):
+                console.print(colored_letters, end="")
+            previous_guesses.append(colored_letters)
 
 
-                # Every time a green letter is flagged, check entire green_check list
-                # To see if all letters are flagged green. If so, user has won.
         print("\nYou have used the following incorrect letters: ")
         print(', '.join(incorrect_letters))
 
+        # Every time a green letter is flagged, check entire green_check list
+        # To see if all letters are flagged green. If so, user has won.
         for check in range(0, len(word)):
             if green_check[check] == 0:
                 losses += 1
